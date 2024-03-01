74b8d07e9d22c9c019d4c8ebe7ea007882ba8afe
Carlos Rivera <cr4185320@gmail.com>
2024-03-01 16:07:10 -0600

    Added FreeType

--- build.bat
+++ build.bat
@@ -2,10 +2,10 @@
 
 IF NOT EXIST build MKDIR build
 SET warning_flags=/WX /W4 /wd4100 /wd4189 /wd4505
-SET includes=/Iext\
+SET includes=/Iext\ /Iext\freetype\
 SET compiler_flags=%warning_flags% %includes% /nologo /FC /Zi /Fdbuild\ /Fobuild\ /FeFocal.exe
 
-SET libs=user32.lib kernel32.lib winmm.lib
-SET linker_flags=%libs% /INCREMENTAL:NO /OPT:REF
+SET libs=user32.lib kernel32.lib winmm.lib freetype.lib
+SET linker_flags=%libs% /LIBPATH:ext\freetype\ /INCREMENTAL:NO /OPT:REF
 
 CL src\win32_focal.cpp %compiler_flags% /link %linker_flags%
