e2f71f5faffd54a7239cee9ec3cebdd3a233146e
Carlos Rivera <cr4185320@gmail.com>
2024-03-03 22:01:14 -0600

    Optional debug/release flags

--- build.bat
+++ build.bat
@@ -1,9 +1,23 @@
 @echo off
 
+set mode=%1
+if [%1]==[] (
+  set mode=debug
+)
+
 IF NOT EXIST build MKDIR build
 SET warning_flags=/WX /W4 /wd4100 /wd4189 /wd4505
 SET includes=/Iext\ /Iext\freetype\
-SET compiler_flags=%warning_flags% %includes% /nologo /FC /Zi /Fdbuild\ /Fobuild\ /FeFocal.exe
+SET compiler_flags=%warning_flags% %includes% /nologo /FC /Fdbuild\ /Fobuild\ /FeFocal.exe
+
+if %mode%==release (
+  SET compiler_flags=/O2 /D %compiler_flags%
+) else if %mode%==debug (
+  SET compiler_flags=/Zi /Od %compiler_flags%
+) else (
+  echo Unkown build mode
+  exit /B 2
+)
 
 SET libs=user32.lib kernel32.lib winmm.lib freetype.lib
 SET linker_flags=%libs% /LIBPATH:ext\freetype\ /INCREMENTAL:NO /OPT:REF
